76ca2f8be2ce42931c3fcab6d95b0898c38303fc
minhnhattrinh <minhnhattrinh312@gmail.com>
2021-12-15 22:01:44 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+I hope you like this !!
+
---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Hân à!',
+    introDesc: "Anh có điều muốn nói với em !!",
+    btnIntro: 'HiHi',
+    title: 'Em có thích anh không :))))😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là thích anh <3',
+    btnNo: 'Không, Anh trai à :) :)',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Bởi vì anh đẹp trai, tài giỏi nhất vũ trụ <3 <3 <3',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều lắm 😘😘',
+    messDesc: 'Em làm bạn gái anh nhé <3 <3 <3',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/messages/t/100004781408421' //your link mess
+}
